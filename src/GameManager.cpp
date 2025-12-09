0c6c61385c82bfa655c9da6861028e3673895f95
302-21\koyo <kobe-license@kobe-tech.ac.jp>
2025-12-09 17:18:39 +0900

    フォルダ構成変更

---
+++ src/GameManager.cpp
@@ -0,0 +1,17 @@
+#include "GameManager.h"
+
+void GameManager::Run()
+{
+	while (true)
+	{
+		this->update();
+		this->draw();
+	}
+}
+
+void GameManager::update()
+{
+}
+void GameManager::draw()
+{
+}
