9fcb13290b8147fda03af93b619eb337f2b53aaf
KDT-KishimotoTakumi <kdt24sgc40002@edu.kobe-tech.ac.jp>
2025-12-12 22:17:48 +0900

    Revert "Windowクラス作成"

--- src/GameManager.h
+++ src/GameManager.h
@@ -1,19 +1,11 @@
 #pragma once
 
 
-#include <memory>
-
-#include "Window.h"
-
 class GameManager
 {
-	//	画面ウィンドウ
-	std::unique_ptr<Window> window;
-
-
 public :
 
-	void Init();
+	
 	void Run();
 
 private:
--- src/Window.h
+++ src/Window.h
@@ -1,30 +1,12 @@
 #pragma once
-#include <vector>
-#include "drawble.h"
-#include "IWindow.h"
 
 //	ウィンドウクラス
-class Window :public IWindow
+class Window
 {
-
-	//	描画リスト
-	std::vector<Drawble> drawList;
 
 public:
 
-
 	//	描画を行うメソッド
-	void Draw();
-
-	//	描画可能オブジェクトを追加するメソッド
-	void AddDrawable(const Drawble& drawable) override;
-
-private:
-
-	//	テキストを描画するメソッド
-	void drawText(const Drawble& drawable);
-
-
 
 };
 
